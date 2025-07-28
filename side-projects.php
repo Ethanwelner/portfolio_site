a918cc89b591667aa281e8b38de8d2307fcd21e0
Ethan <ewelner22@gmail.com>
2025-07-27 21:39:23 -0400

    fixed social links.php page

---
+++ side-projects.php
@@ -0,0 +1,106 @@
+<!doctype html>
+<html lang="en">
+
+    <head>
+        <!-- this loads the head code -->
+        <?php include 'php/head-loads.php'; ?>
+        <title>Ethan Welner Portfolio</title>
+    </head>
+
+    <body>
+
+        <!-- Main Content -->
+        <!------------ side projects black section ------------>
+        <div class="section short-section black-bg d-flex black-section z-4">
+            <div class="section-clip d-flex align-items-center justify-content-center">
+                <!-- This loads the header -->
+                <?php include 'php/header.php'; ?>
+                <div class="title section-title d-inline-flex z-2">
+                    <div class="hero-statement pre-slide-in">
+                        <!-- <h3 class="copy-title non-title"><img class="warning-gif" src="img/warning.gif"/>Site Under Construction</h3> -->
+                        <h1 class="title hero-title white-text ">Digitally Assisted PNP RPG</h1>
+                        <!-- <h2 class="title section-title white-text">User Interface and Experience</h2>
+                        <h3 class="sub-title white-text non-title">The Mathematics of Design</h3> -->
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="section unlimited white-bg d-flex">
+                <a href="index.php" class="section-logo-bug black-bg repaint z-3">EW</a>
+                <div class="section-about-social-container z-3">
+                    <!-- social links -->
+                    <a target="_blank" href="https://www.linkedin.com/in/ethan-welner-555b4055/">
+                        <div class="social-link black-bg repaint">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448.1 512">
+                                <path d="M100.3 448H7.4V148.9h92.9V448zM53.8 108.1C24.1 108.1 0 83.5 0 53.8S24.1 0 53.8 0s53.8 24.1 53.8 53.8-24.1 54.3-53.8 54.3zM448 448h-92.7V302.4c0-34.7-.7-79.2-48.3-79.2-48.3 0-55.7 37.7-55.7 76.7V448h-92.8V148.9h89.1v40.8h1.3c12.4-23.5 42.7-48.3 87.9-48.3 94 0 111.3 61.9 111.3 142.3V448h-.1z"/>
+                            </svg>
+                        </div>
+                    </a>
+                    <a target="_blank" href="https://www.instagram.com/shumagram/">
+                        <div class="social-link black-bg repaint">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                                <path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"/>
+                            </svg>
+                        </div>
+                    </a>
+                    <!-- about me link -->
+                    <div class="section-about-me black-text repaint about-me-link">About Me</div>
+                </div>
+
+
+                <div class="container d-flex z-2 flex-wrap mt-5 mt-lg-0">
+
+                    <div class="d-flex flex-wrap flex-xs-nowrap">
+                        <div class="flex-2 d-none d-xl-inline-flex"></div>
+                        <div class="d-inline-flex align-items-center flex-7">
+                            <div>
+                                <h3 class="sub-title mb-3">Meazure Learnings Responsivity and Accessibility Redesign</h3>
+                                <div class="copy stinger red mb-3">A Design and Front End Overhaul To Bring Responsivity and Accessibility</div>
+                                <p class="copy">
+                                    Meazure Learning is an end-to-end online proctoring and testing platform provider that helps academic students and career professionals take exams and assessments from their homes. With today's heavy emphasis on accessibility and interoperability, the front end of the platform needed to become responsive and accessible. In addition, the platform needed a significant overhaul to many of its functions and components to declutter and provide an intuitive interface for a wide variety of users.
+                                    <br><br>
+                                    This was no small task; the platform had existed for over a decade as a continuously iterated-upon product with legacy requirements and quite a few skeletons in its closet. To that end, we embarked on a lengthy journey to assess user pain points, scope the full need for accessibility (full WCAG 2.2 compliance), and fulfill users' needs regardless of the device they had access to. Along the way we rebased and refactored the front end of the platform, adopted a new design standard, and implemented a newly improved component library.
+                                    <br><br>
+                                    By the end of the research, design, development, and testing process, the platform was WCAG certified, wholly responsive, and rebranded to fit within the modern company identity (it performed better too!).
+                                </p>
+                            </div>
+                        </div>
+                        <div class="d-inline-flex align-items-center flex-1"></div>
+                        <div class="d-inline-flex flex-6 justify-content-end align-items-center">
+                            <img class="ux-iso shadow-diffuse" src="img/respon_1.jpg">
+                        </div>
+                        <div class="flex-2 d-none d-xl-inline-flex"></div>
+                    </div>
+                    
+
+                    <div class="separator"></div>
+
+
+
+                    <div class="meazure-gallery-container shadow-diffuse gray-bg breakout z-1">
+                        <div class="meazure-gallery">
+                            <img class="m-g-laptop" src="img/respon_laptop.png">
+                            <div class="m-g-mobile-container">	
+                                <img class="m-g-mobile" src="img/respon_mobile_1.png">
+                                <img class="m-g-mobile" src="img/respon_mobile_2.png">
+                            </div>
+                        </div>
+                    </div>
+
+
+                </div>
+
+            </div>
+
+		<!-- this loads the page footer for both mobile and desktop -->
+		<?php include 'php/footer.php'; ?>
+
+		<!-- this loads the page footer for both mobile and desktop -->
+		<?php include 'php/about.php'; ?>
+
+		<!-- this loads the javascript plugins for each page -->
+		<?php include 'php/javascript-loads.php'; ?>
+        
+    </body>
+</html> 
